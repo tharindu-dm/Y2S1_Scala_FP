a1738e443a076860ae31588a465e069b82985735
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2024-07-01 18:09:49 +0530

    Add files via upload

--- 22001281_Lab3/q3.scala
+++ 22001281_Lab3/q3.scala
@@ -1,18 +1,6 @@
-def floater(n1:Int, n2:Int):Float = {
-    var avg:Float = 511.97604 //(n1 + n2).toFloat / 2   //511.97604
-    var interger = avg.floor.toInt        //511
-    var floatDiff = avg - interger   //0.97604
-
-    var finalFloat = (floatDiff * 100).floor.toInt //97
-    var value = (floatDiff * 1000).floor.toInt - ((finalFloat) * 10) //(976)-970 = 6
-    
-    if(value >= 5)
-    {
-        finalFloat += 1; //97 -> 98
-    }
-    
-    var result = finalFloat.toFloat/(100f)
-    return interger.toFloat + result
+def floater(n1:Int, n2:Int) = {
+    var avg:Float = (n1 + n2).toFloat / 2
+    printf("%.2f\n", avg)
 }
 
 object q3 {
@@ -20,7 +8,7 @@ object q3 {
         val num1:Int = 96
         val num2:Int = 43
 
-        println(floater(num1, num2))
+        floater(num1, num2)
     }
 }
 
