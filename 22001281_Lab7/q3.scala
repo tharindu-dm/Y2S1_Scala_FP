365d8c303fc1d366037b8a0b037f419eca41aa21
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2024-08-04 12:02:59 +0530

    Update q3.scala

--- 22001281_Lab7/q3.scala
+++ 22001281_Lab7/q3.scala
@@ -1,12 +1,3 @@
-/**
-  *  Write a Scala function called filterPrime that takes a list of integers as input and uses a lambda
- function to filter out the prime numbers from the list. The function should return a new list
- containing only the prime numbers.
- Example:
- Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- Output: [2, 3, 5, 7]
-  */
-
 object q3 {
     def main(args: Array[String]): Unit = {
         val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
