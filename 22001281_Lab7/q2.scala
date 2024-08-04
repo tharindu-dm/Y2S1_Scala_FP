31a68237f585e16f4239506d163123ec4a62e9a7
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2024-08-04 12:02:05 +0530

    Update q2.scala

--- 22001281_Lab7/q2.scala
+++ 22001281_Lab7/q2.scala
@@ -1,14 +1,3 @@
-/**
-  * Write a Scala function called calculateSquare that takes a list of integers as input and uses a
- lambda function to calculate the square of each number in the list. The function should return a
- newlist containing the squares of the input numbers in the same order.
- Example:
- Input: [1, 2, 3, 4, 5]
- Output: [1, 4, 9, 16, 25]
- You can use the map method onlists to implement the calculateSquare function with a lambda
- function inside it.
-  */
-
 object q2 {
   def main(args: Array[String]): Unit = {
     val list = List(1, 2, 3, 4, 5)
