214e9cf90eeabf66a55449756f229feb702e0c6e
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2024-08-12 23:50:49 +0530

    added comments and improved

--- 22001281_Lab8/q1.scala
+++ 22001281_Lab8/q1.scala
@@ -1,34 +1,34 @@
 /**
-  *  The Caesar cipher is one of the earliest known and simplest ciphers. It is a type of substitution
- cipher in which each letter in the plaintext is 'shifted' a certain number of places down the
- alphabet. For example, with a shift of 1, A would be replaced by B, B would become C, and
- so on. The method is named after Julius Caesar, who apparently used it to communicate
- with his generals.
- a. Implement Encryption and Decryption functions of Caesar cipher.
- b. Then implement a Cipher function which takes Encryption and Decryption functions
- to process the data.
+  *  The Caesar cipher 
   */
 
 object q1 {
   def main(args: Array[String]): Unit = {
-    val encrypt = (c: Char, key: Int) => {
-      if (c.isLetter) {
-        val base = if (c.isUpper) 'A' else 'a'
-        ((c - base + key) % 26 + base).toChar
-      } else c
+
+    val encrypt: (Char, Int) => Char = (c, key) => {
+      if (c.isLetter) 
+      { // Check if the character is a letter
+        val base = if (c.isUpper) 'A' else 'a' // Determine base (A or a)
+        val shift = (c - base + key) % 26 
+
+        val normalizedOffset = if (shift < 0) shift + 26 else shift // Adjust if negative
+        (normalizedOffset + base).toChar // Convert to character
+      } else c // Non-letter characters remain unchanged
     }
 
-    val decrypt = (c: Char, key: Int) => encrypt(c, 26 - key)
+    val decrypt = (c: Char, key: Int) => encrypt(c, 26 - key) //reversed of encrypt
 
     val cipher = (algo: (Char, Int) => Char, text: String, key: Int) => text.map(algo(_, key))
 
-    val text = "Hello, World!"
-    val key = 5
+    val text = "The Quick Brown Fox Jumps Over The Lazy Dog."
+
+    val key = 5           //tell how many places to shift towards right (positive) or left (negative)
     val encryptedText = cipher(encrypt, text, key)
+
     val decryptedText = cipher(decrypt, encryptedText, key)
 
-    println(s"Original text: $text")
+    println(s"\nOriginal text: $text\n")
     println(s"Encrypted text: $encryptedText")
-    println(s"Decrypted text: $decryptedText")
+    println(s"Decrypted text: $decryptedText\n")
   }
 }
--- 22001281_Lab8/q2.scala
+++ 22001281_Lab8/q2.scala
@@ -1,12 +1,6 @@
 /*
-*  Write a Scala program that takes an integer input from the command line. Based on the
- input, use pattern matching to categorize the number and print the corresponding message.
- Implement the following logic using lambda functions:
- ● Iftheinput is a multiple of 3, print "Multiple of Three".
- ● Iftheinput is a multiple of 5, print "Multiple of Five".
- ● Iftheinput is a multiple of both 3 and 5, print "Multiple of Both Three and Five".
- ● Iftheinput is none of the above, print "Not a Multiple of Three or Five".
-* */
+*  "Multiple of Both Three and Five".
+*/
 
 object q2 {
   def main(args: Array[String]): Unit = {
